77dec0e593a5387445c1ad35cf3482b69431b03b
github-action-benchmark <github@users.noreply.github.com>
2022-01-29 23:09:25 +0000

    add C++ Benchmark (googlecpp) benchmark result for ee775979ef2f9dc7c66e4a37161194674669e467

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1643497765942,
+  "repoUrl": "https://github.com/gregbartell/wordler",
+  "entries": {
+    "C++ Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "gregorybartell@gmail.com",
+            "name": "Gregory Bartell",
+            "username": "gregbartell"
+          },
+          "committer": {
+            "email": "gregbartell@users.noreply.github.com",
+            "name": "gregbartell",
+            "username": "gregbartell"
+          },
+          "distinct": true,
+          "id": "ee775979ef2f9dc7c66e4a37161194674669e467",
+          "message": "Fix Build Github Action",
+          "timestamp": "2022-01-29T16:58:24-06:00",
+          "tree_id": "d72315e95890c147213326b73526377a74a34a11",
+          "url": "https://github.com/gregbartell/wordler/commit/ee775979ef2f9dc7c66e4a37161194674669e467"
+        },
+        "date": 1643497765550,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_SolverGetBestGuess/100",
+            "value": 74798253.55555648,
+            "unit": "ns/iter",
+            "extra": "iterations: 9\ncpu: 74784788.8888889 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_WordleGetHint/1000",
+            "value": 505867761.99999404,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 504895899.99999994 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
